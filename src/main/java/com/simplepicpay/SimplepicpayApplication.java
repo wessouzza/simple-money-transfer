94722721473ca85bdd4935d62adbb7263ee41a3e
wessouzza <wes-souza@hotmail.com>
2023-12-01 18:04:10 -0300

    First commit

---
+++ src/main/java/com/simplepicpay/SimplepicpayApplication.java
@@ -0,0 +1,13 @@
+package com.simplepicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplepicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplepicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplepicpay/SimplepicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplepicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplepicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
